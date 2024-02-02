0ec6470f55e33d5af2c6223b2e0f6e57a4622f83
Russ Perry <rperry1886@gmail.com>
2024-02-02 14:14:48 -0500

    Add Comments to Code

--- beer.py
+++ beer.py
@@ -1,13 +1,16 @@
+# Import Statements
 import json
 import requests
 from random import randint
 
+# Get user input, and run a query on the API using the input.
 food_choice = input('Please enter your dinner choice: ')
 url = f'https://api.punkapi.com/v2/beers?food={food_choice}'
 
 r = requests.get(url)
 data = json.loads(r.text)
 
+# Create a recommended beer list array based on user input.
 beer_list = []
 
 for beer in data:
@@ -25,15 +28,17 @@ for beer in data:
 
     beer_list.append(beer_item)
 
+# Generate a random recommendation from the beer list array
 value = randint(0,len(beer_list))
-
 recommendation = beer_list[value]
 
+# Set each piece of the recommendation to it's own variable for nicer output.
 rec_name = recommendation['name']
 rec_tagline = recommendation['tagline']
 rec_abv = recommendation['abv']
 rec_desc = recommendation['desc']
 
+# Display the recommended beer.
 print("If you are ordering " + food_choice + ", then might we suggest " + rec_name + "." + " " + rec_tagline)
 print(rec_desc)
 print("The ABV is " + str(rec_abv) + ".")
